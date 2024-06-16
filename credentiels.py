0d2170f5fab7673d2b8519ad723e8380652ddf89
Scoth-TheDev <hamedbenarous00@gmail.com>
2024-06-16 23:32:35 +0200

    add credentiels

---
+++ credentiels.py
@@ -0,0 +1,4 @@
+twitter_email = "hamedbenarous00@gmail.com"
+twitter_username = "Scothy91"
+twitter_password = "Iceboy123_"
+openai_api_key = "sk-proj-LcjGZWmMPvprmP9wyXhET3BlbkFJNFLB9HoBxSgGEXJwv1Ot"
