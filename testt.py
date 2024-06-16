bb5ddd6b424015a2821b5c33d2a712dc188b5d9a
Scoth-TheDev <62766093+Scoth-TheDev@users.noreply.github.com>
2024-06-16 22:22:17 +0200

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,1 +1,12 @@
- 
+import streamlit as st
+
+def main():
+    st.title("Welcome to Streamlit")
+    st.header("This is a simple Streamlit app!")
+    st.subheader("Try changing the value below:")
+    
+    name = st.text_input("Enter your name", "John Doe")
+    st.write("Hello,", name, "!")
+
+if __name__ == '__main__':
+    main()
